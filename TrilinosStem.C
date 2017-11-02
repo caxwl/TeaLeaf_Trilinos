76896f2fd9438dc52bcb9567bc260a8af575a268
caxwl <caxwl@hotmail.com>
2017-11-02 11:19:51 +0000

    Cleanup headers for 12.12.1

--- TrilinosStem.C
+++ TrilinosStem.C
@@ -37,13 +37,6 @@
 #include "BelosOperatorT.hpp"
 
 // Xpetra
-#include <Xpetra_Map.hpp>
-#include <Xpetra_MapFactory.hpp>
-#include <Xpetra_CrsMatrixWrap.hpp>
-#include <Xpetra_VectorFactory.hpp>
-#include <Xpetra_MultiVectorFactory.hpp>
-#include <Xpetra_Parameters.hpp>
-
 #include <Xpetra_Matrix.hpp>
 #include <MueLu_CoordinatesTransferFactory.hpp>
 #include <Xpetra_TpetraMultiVector.hpp>
